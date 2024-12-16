02fa565adb5e23bbd704427f08449ebe2ba6c089
SuleymanAtmaca12 <127023748+SuleymanAtmaca12@users.noreply.github.com>
2024-12-16 11:21:18 +0000

    requirements

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'GörselAlgılama',
+    version= '0.0.0',
+    author= 'Süleyman Atmaca',
+    author_email= 'atamca34suleyman@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
